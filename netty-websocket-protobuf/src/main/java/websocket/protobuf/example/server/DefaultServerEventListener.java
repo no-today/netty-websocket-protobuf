a6e4efc8764efa26657015e466b4bb2ecfc0a4fe
notoday <no.today@outlook.com>
2021-03-03 14:00:43 +0800

    fix: 修复空指针

--- netty-websocket-protobuf/src/main/java/websocket/protobuf/example/server/DefaultServerEventListener.java
+++ netty-websocket-protobuf/src/main/java/websocket/protobuf/example/server/DefaultServerEventListener.java
@@ -8,8 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import websocket.protobuf.example.protocol.ProtobufMessageModule;
 import websocket.protobuf.example.security.Authenticator;
+import websocket.protobuf.example.security.Cookie;
 import websocket.protobuf.example.server.support.ChannelAttr;
 import websocket.protobuf.example.service.CommandStrategyFactory;
+
+import java.util.Optional;
 
 /**
  * @author no-today
@@ -34,37 +37,41 @@ public class DefaultServerEventListener implements ServerEventListener {
         ChannelAttr.setSession(channel, authenticator.authentication(channel, req));
         channelGroup.add(channel);
 
-        log.debug("[BeforeHandshake]: uid: {}", ChannelAttr.getSession(channel).getPrincipal());
+        log.debug("[BeforeHandshake]: uid: {}", getPrincipal(channel));
     }
 
     @Override
     public void doOnOpen(Channel channel, FullHttpRequest req) {
-        log.debug("[OnOpen]: uid: {}", ChannelAttr.getSession(channel).getPrincipal());
+        log.debug("[OnOpen]: uid: {}", getPrincipal(channel));
     }
 
     @Override
     public void doOnClose(Channel channel) {
-        log.debug("[OnClose]: uid: {}", ChannelAttr.getSession(channel).getPrincipal());
+        log.debug("[OnClose]: uid: {}", getPrincipal(channel));
     }
 
     @Override
     public void doOnError(Channel channel, Throwable throwable) {
-        log.error("[OnError]: uid: {}", ChannelAttr.getSession(channel).getPrincipal(), throwable);
+        log.error("[OnError]: uid: {}", getPrincipal(channel), throwable);
     }
 
     @Override
     public void doOnEvent(Channel channel, Object event) {
-        log.debug("[OnEvent]: uid: {}, event: {}", ChannelAttr.getSession(channel).getPrincipal(), event);
+        log.debug("[OnEvent]: uid: {}, event: {}", getPrincipal(channel), event);
     }
 
     @Override
     public void doOnTextMessage(Channel channel, TextWebSocketFrame frame) {
-        log.debug("[OnTextMessage]: uid: {}, text: {}", ChannelAttr.getSession(channel).getPrincipal(), frame.text());
+        log.debug("[OnTextMessage]: uid: {}, text: {}", getPrincipal(channel), frame.text());
     }
 
     @Override
     public void doOnBinaryMessage(Channel channel, ProtobufMessageModule.Message frame) {
-        log.debug("[OnBinaryMessage]: uid: {}, command: {}", ChannelAttr.getSession(channel).getPrincipal(), frame.getCommand());
+        log.debug("[OnBinaryMessage]: uid: {}, command: {}", getPrincipal(channel), frame.getCommand());
         commandStrategyFactory.execute(channel, frame);
+    }
+
+    private String getPrincipal(Channel channel) {
+        return Optional.ofNullable(ChannelAttr.getSession(channel)).map(Cookie::getPrincipal).orElse("Anonymous");
     }
 }
